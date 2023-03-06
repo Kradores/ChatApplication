85d469d040e170e3b3297b1a907d914e3adec794
Alexandru Noroc <alexandru.noroc@amdaris.com>
2023-03-07 00:59:18 +0200

    WIP: add create chat page

---
+++ BlazorChat/Client/Models/Chat/ChatCreateInput.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorChat.Client.Models.Chat;
+
+public class ChatCreateInput
+{
+    public string Name { get; set; } = null!;
+    public List<UserCheckbox> UserCheckboxes { get; set; } = new List<UserCheckbox>();
+
+    public class UserCheckbox
+    {
+        public string Name { get; set; } = null!;
+        public string Id { get; set; } = null!;
+        public bool IsChecked { get; set; }
+    }
+}
